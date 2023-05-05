9bf575cca427fe21e6f9c80918ea92ea70eba43b
haidarcse2018 <haidarcse2017@gmail.com>
2023-05-06 03:27:26 +0600

    user-auth create, signup ,login component create, and desgin also add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {UserAuthModule} from './user-auth/user-auth.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
