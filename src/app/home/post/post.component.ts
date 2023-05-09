a0f5cad89c7e70ed67a4b5a3763cbce3424802f3
haidarcse2018 <haidarcse2017@gmail.com>
2023-05-09 16:45:44 +0600

    comment show

--- src/app/home/post/post.component.ts
+++ src/app/home/post/post.component.ts
@@ -16,10 +16,22 @@ export class PostComponent implements OnInit {
   commentFormData: any;
   signUpFormData: any;
   c_comment:any;
+  c_user:any;
+  todaysdate:any;
+  postId:any;
  
   constructor(private userDataservice: UserdataService,private route:ActivatedRoute,private http: HttpClient,private router: Router,private sharedService: SharedServiceService) { 
     this.commentFormData = this.sharedService.getcommentFormData();
-    this.signUpFormData = this.userDataservice.getSignUpFormData();
+    this.signUpFormData = this.userDataservice.getSignUpFormData()
+    
+      //  const c_comment:any=commentData[0] ;
+      //  const c_postId:any=commentData[1] ;
+      //  const c_user:any=commentData[2] ;
+      //  const c_pass:any=commentData[3] ;
+
+      //  const s_user:any=signupData[0] ;
+      //  const s_pass:any=signupData[1] ;
+  
 
         // console.warn("comment data:",this.sharedService.getcommentFormData());
         // console.warn("Signup  data:",this.userDataservice.getSignUpFormData());
@@ -34,7 +46,8 @@ export class PostComponent implements OnInit {
 
 
   fetchPostDetails() {
-    const postId =this.route.snapshot.paramMap.get('id');   
+    const postId =this.route.snapshot.paramMap.get('id'); 
+    // const postId =this.route.snapshot.paramMap.get('id');   
     //  console.warn(postId);
     const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;  
     this.http.get(url).subscribe((response) => {
@@ -43,16 +56,34 @@ export class PostComponent implements OnInit {
 
       //  console.warn('comment data',this.commentFormData); 
 
+        //  console.warn(this.signUpFormData.username,this.signUpFormData.password);
+      if(this.commentFormData){
+        // console.warn('comment data postid',this.commentFormData.postId );
+        // console.warn(postId);
+        const c_password=this.commentFormData.pasword;
        const commentData = Object.values(this.commentFormData);
        const signupData = Object.values(this.signUpFormData);
+        // console.warn('comment data',commentData[0]); 
+        // console.warn('comment postid',commentData[1]);
+        // console.warn('comment user',commentData[2]);
+        // console.warn('comment pass',commentData[3]);
+        if(commentData[2] === signupData[0]  && this.commentFormData.postId===postId){
+          this.c_comment=commentData[0] ;
 
-       const c_comment:any=commentData[0] ;
-       const c_postId:any=commentData[1] ;
-       const c_user:any=commentData[2] ;
-       const c_pass:any=commentData[3] ;
+          this.c_user=commentData[2] ;
+          this.todaysdate=Date();
+        }
 
-       const s_user:any=signupData[0] ;
-       const s_pass:any=signupData[1] ;
+        
+      //  const c_postId:any=commentData[1] ;
+      
+      //  const c_pass:any=commentData[3] ;
+
+      //  const s_user:any=signupData[0] ;
+      //  const s_pass:any=signupData[1] ;
+      }
+
+     
   
 
 
@@ -65,7 +96,14 @@ export class PostComponent implements OnInit {
     // alert(eventData);
     const postId =this.route.snapshot.paramMap.get('id'); 
     // alert(postId);
-    this.router.navigate(['/addcomment', postId]);
+
+     
+    if(this.signUpFormData){
+      this.router.navigate(['/addcomment', postId]);
+    }else{
+      this.router.navigate(['/login']);
+    }
+    
   }
 
 }
